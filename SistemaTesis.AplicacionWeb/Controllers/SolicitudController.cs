a925d609379bb55eec63d624f08d9556a396e3cb
fperez <PEREZFAQ98-2020@ies21.onmicrosoft.com>
2022-11-01 19:53:21 -0300

    sync

---
+++ SistemaTesis.AplicacionWeb/Controllers/SolicitudController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaTesis.AplicacionWeb.Controllers
+{
+    public class SolicitudController : Controller
+    {
+
+        public IActionResult Solicitudes()
+        {
+            return View();
+        }
+
+        public IActionResult NuevaSolicitud()
+        {
+            return View();
+        }
+
+        public IActionResult HistorialSolicitudes()
+        {
+            return View();
+        }
+    }
+}
